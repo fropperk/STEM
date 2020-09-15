a92b983f6a6f4265de90a3df8423ef63d286751d
fropperk <71330675+fropperk@users.noreply.github.com>
2020-09-15 15:22:09 +0200

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,22 @@
+print("This is the Var exercise")
+bigVar = 345
+lilVar = 234  # creates the 2 values that we use in math
+print(bigVar)
+print(lilVar)  # prints out the values
+comboVar = bigVar + lilVar  # adds the two values and creates a sum variable
+print(comboVar)  # prints out the var
+subtractVar = bigVar - lilVar  # subtract
+multiVar = bigVar * lilVar  # multiply
+divVar = bigVar / lilVar  # division
+print(subtractVar, " ", multiVar, " ", divVar)  # prints out the values of multiplication adn subtraction adn division
+print("This is the end of var exercise")
+print("---------------------------------------------------------")
+print("This is the string exercise")
+firstName = "Adam"  # defines my first name
+scndName = "Prenosil"  # defines my second name
+print(firstName, " ", scndName)  # this prints my name using the 2 strings
+mahName = firstName + " " + scndName  # this is my full name which is a comb of firstName and scndName
+print(mahName)  # prints my name which is the sum of firstName and scndName
+print(mahName * 2)  # prints the name two times like this: Adam PrenosilAdam Prenosil
+print("This is the end of string exercise")
+print("---------------------------------------------------------")
